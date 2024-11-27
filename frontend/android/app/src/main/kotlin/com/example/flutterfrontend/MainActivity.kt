4aab71ee3b82cd4e1c4912ccfcf390f935fa0cec
Nova-lotus <63787984+Nova-lotus@users.noreply.github.com>
2024-11-27 18:13:58 +0200

    chore: Remove all frontend and backend files, including environment configurations, logs, and assets, in preparation for a complete project overhaul.

--- frontend/android/app/src/main/kotlin/com/example/flutterfrontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutterfrontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- frontend/lib/utils/constants.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-final String apiUrl = dotenv.get('API_URL', fallback: 'http://localhost:8080');
